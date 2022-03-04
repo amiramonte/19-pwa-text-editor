563ba2408d799e25234df8f40a0675f818aae0ea
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-03-03 16:40:52 -0800

    added more tasks to pseudo code file

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -21,11 +21,12 @@
     // 
 // 
 // 
-// 
-    // 
-    // 
-    // 
-    // 
+// Webpack.config.js file
+    // make sure everything needed is imported
+    // workbox plugin for service worker
+    // workbox plugin for manifest file
+    // add CSS loaders
+    // add babel
     // 
     // 
     // 
