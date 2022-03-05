86058e1c331360918351c0fba0b66d23f38ca48d
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-03-04 19:51:41 -0800

    updated pseudocode file to reflect remaining tasks

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -1,14 +1,3 @@
-// src-sw.js
-// remove offlineFallback from workbox
-// implement asset caching
-//
-//
-//
-//
-//
-//
-//
-//
 //
 // Webpack.config.js file
 // make sure everything needed is imported
