b82c5bebbd5d716024166d9d15ba067325a99685
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-03-04 18:22:51 -0800

    updated pseudocode file

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -1,48 +1,48 @@
 // client folder => src folder => js folder => js folder => database.js file
-    // method that accepts data and adds to database
-    // method that gets content from the database
-    // 
-    // 
-// install.js file 
-    // add logic for installing PWAs
-    // 
-    // 
-// 
-// 
+// method that accepts data and adds to database
+// method that gets content from the database
+//
+//
+// install.js file
+// add logic for installing PWAs
+//
+//
+//
+//
 // src-sw.js
-    // remove offlineFallback from workbox
-    // implement asset caching
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-// 
-// 
+// remove offlineFallback from workbox
+// implement asset caching
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // Webpack.config.js file
-    // make sure everything needed is imported
-    // workbox plugin for service worker
-    // workbox plugin for manifest file
-    // add CSS loaders
-    // add babel
-    // 
-    // 
-    // 
-    // 
-// 
-// 
-// 
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-    // 
-// 
-// 
-// 
-// 
+// make sure everything needed is imported
+// workbox plugin for service worker
+// workbox plugin for manifest file
+// add CSS loaders
+// add babel
+//
+//
+//
+//add scripts to package.json
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//deploy to Heroku
