cb800eff8bf495c3544900b11f144f95ea05d561
Alex Miramontes <alex.r.miramontes@gmail.com>
2022-03-06 19:02:52 -0800

    removed Assets folder with old image files no longer needed, updated README with Description, Technologies, Deployment and Credits sections, updated pseudocode.js file with remaining tasks

--- z-pseudocode.js
+++ z-pseudocode.js
@@ -1,4 +1,14 @@
 //
 // README completion
+    // gif of website functioning from heroku for functionality section
+    // link to deployed text editor on heroku in new section
+    //
+    //
+    //
+    //
+    //
+    //
+//
+//
 //
 // SUBMIT
